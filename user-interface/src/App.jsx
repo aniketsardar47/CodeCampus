b26e6f515cb9c5c4d9d159fb05c8219451c2bc93
Varun Tipkari <varuntipkari123@gmail.com>
2025-03-19 23:45:37 +0530

    Folder Structure

---
+++ user-interface/src/App.jsx
@@ -0,0 +1,13 @@
+import { Heading } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <Heading p={10}>CodeCampus</Heading>
+    </>
+  )
+}
+
+export default App
