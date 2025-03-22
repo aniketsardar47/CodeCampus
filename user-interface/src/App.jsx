6b378b7ec105155cd7afd2beddf2f7678228b845
jayesh-borase <borsejayesh069@gmail.com>
2025-03-22 12:24:26 +0530

    login register page

--- user-interface/src/App.jsx
+++ user-interface/src/App.jsx
@@ -1,12 +1,20 @@
 import { Heading } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './pages/Home/Home'
+import Stud_Form from './Login_Page/Login_Panel'
 
 function App() {
   
   return (
     <>
-        <Home/>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Stud_Form />} />
+        <Route path="/dashboard" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+       
     </>
   )
 }
