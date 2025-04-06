8981da929260f05aa9552e79d541eec81929fe8e
aniketsardar47 <aniketsardar89@gmail.com>
2025-04-06 11:00:54 +0530

    Corrected Navbar

--- user-interface/src/pages/Editor/navbar.jsx
+++ user-interface/src/pages/Editor/navbar.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import { Box, Flex, Heading, Button, Spacer } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ darkMode }) => {
+  const bgColor = darkMode ? 'gray.800' : 'gray.100';
+  const textColor = darkMode ? 'white' : 'gray.800';
+  const buttonVariant = darkMode ? 'outline' : 'solid';
+  const buttonColorScheme = darkMode ? 'gray' : 'blue';
+
   return (
-    <Box bg="gray.800" px={8} py={5} color="white">
+    <Box bg={bgColor} px={8} py={5} color={textColor}>
       <Flex align="center">
-        <Heading size="md">Code Editor</Heading>
+        <Heading size="md" color={textColor}>Code Editor</Heading>
         <Spacer />
         <Flex gap={4}>
           <Link to="/">
-            <Button padding={2}
-                    colorPalette="grey"
-                    variant="outline" >Home</Button>
+            <Button
+              padding={2}
+              colorScheme={buttonColorScheme}
+              variant={buttonVariant}
+            >
+              Home
+            </Button>
           </Link>
-          <Button padding={2}
-                  colorPalette="grey"
-                  variant="outline">Sign Out</Button>
+          <Button
+            padding={2}
+            colorScheme={buttonColorScheme}
+            variant={buttonVariant}
+          >
+            Sign Out
+          </Button>
         </Flex>
       </Flex>
     </Box>
