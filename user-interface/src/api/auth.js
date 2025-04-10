5cac9fc2d89f3fc77bf69945724b3235419f7e23
Varun Tipkari <varuntipkari123@gmail.com>
2025-04-10 10:52:18 +0530

    server url updated

--- user-interface/src/api/auth.js
+++ user-interface/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL = "https://codecampus-server-tp47.onrender.com/api/auth";
 
 export const register = async (userData) => {
     try{
